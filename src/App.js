df15c3860715b68f234c676b0ce824f3f8c61606
Gökhan Yılmaz <gokhanyilmaz997@gmail.com>
2022-02-17 20:47:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [message, setMessage] = useState("Start guessing...");
+  const [number, setNumber] = useState("");
+  const [score, setScore] = useState(20);
+  const [highscore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    setNumber(randomNumber());
+  }, []);
+
+  const randomNumber = () => {
+    return Math.trunc(Math.random() * 20) + 1;
+  };
+
+  const againGame = () => {
+    setNumber(randomNumber());
+    setInput("");
+    setMessage("Start guessing...");
+    document.body.style.background = "#222";
+    setScore(20);
+  };
+
+  const handleChange = (e) => {
+    setInput(Number(e.target.value));
+  };
+
+  const gameMessage = () => {
+    if (!input) {
+      setMessage("No Number!");
+    } else if (number === input) {
+      setMessage("Corrent Number!");
+      document.body.style.background = "green";
+      if (highscore < score) {
+        setHighScore(score);
+      }
+    } else if (number !== input) {
+      if (score > 1) {
+        number > input ? setMessage("Too Low !") : setMessage("Too High !");
+        console.log({ number });
+        console.log({ input });
+        setScore(score - 1);
+      } else {
+        setMessage("You Lost!");
+        document.body.style.background = "red";
+      }
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Guess My Number!</h1>
+        <p className="between">(Between 1 and 20)</p>
+        <button className="btn again" onClick={againGame}>
+          Again!
+        </button>
+        <div className="number" id="number1">
+          {" "}
+          ?{" "}
+        </div>
       </header>
+      <main>
+        <section className="left">
+          <input
+            type="number"
+            value={input}
+            className="guess"
+            onChange={handleChange}
+          />
+          <button className="btn check" onClick={gameMessage}>
+            Check!{" "}
+          </button>
+        </section>
+        <section className="right">
+          <p className="message">{message}</p>
+          <p className="label-score">
+            💯 Score: <span className="score"> {score}</span>
+          </p>
+          <p className="label-highscore">
+            🥇 Highscore: <span className="highscore">{highscore}</span>
+          </p>
+        </section>
+      </main>
     </div>
   );
 }
